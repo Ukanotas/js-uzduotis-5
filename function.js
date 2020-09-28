61632bddd30c746b8a334231ed6b45f8bbb4eb7f
Ukanotas <simonas.rukas@gmail.com>
2020-09-29 01:01:07 +0300

    added rules for format of variables in function

--- function.js
+++ function.js
@@ -2,7 +2,20 @@
 //b - kas kelinta raide isrinkti
 
 function isrinktiRaides(a, b) {
-    if (typeof val !== 'string') {
-        
+    if (typeof a !== 'string') {
+        console.log('Pirmasis kintamasis yra netinkamo tipo.');
+    }  else if (a === '') {
+        console.log('Pirmojo kintamojo reikšmė yra netinkamo dydžio');
+    }  else if (a.length > 5) {
+        console.log('Pirmojo kintamojo reikšmė yra netinkamo dydžio');
+    }
+
+    if (typeof b !== 'number') {
+        console.log('Antrasis kintamasis yra netinkamo tipo');
+    } else if (b <= 0) {
+        console.log('Antrasis kintamasis turi būti didesnis už nulį.');
     }
 }
+
+
+console.log(isrinktiRaides('qweradifajosid', 10));
